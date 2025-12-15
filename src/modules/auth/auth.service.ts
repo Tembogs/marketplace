c0710fe09082690b4089065fb4a846cc2d7f3823
Tembogs <temmytizzy201@gmail.com>
2025-12-15 14:16:19 +0100

    live chat

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import prisma from "../config/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "supersecret" 
+
+export class AuthService {
+  static async register(
+    email:string, 
+    password:string,
+    role: "USER" | "EXPERT"
+  ) {
+    const existing = await prisma.user.findUnique({where: {email}});
+    if (existing) throw new Error("Email already in use");
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const user = await await prisma.user.create({
+      data: {email, passwordHash, role}
+    })
+
+    const token = jwt.sign({userId: user.id, role: user.role}, JWT_SECRET, {expiresIn: "7d"})
+    return {user, token};
+  }
+
+
+  static async login(
+    email:string,
+    password:string
+  ){
+    const user = await prisma.user.findUnique({where: {email}})
+  if (!user) throw new Error("Invalid creddntials")
+
+    const isValid = await bcrypt.compare(password, user.passwordHash);
+    if (!isValid) throw new Error("Invalid password") 
+    
+    const token = jwt.sign({userId: user.id, role:user.role}, JWT_SECRET, {expiresIn:"7d"});
+    return {user, token}
+  }
+
+}
